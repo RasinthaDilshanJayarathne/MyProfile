2e05ccf6d35bb82fc4e9c1a788eb4326afe4e095
Radintha Dilshan <rasinthadilshan9@gmail.com>
2022-05-24 10:24:39 +0530

    'Adding Social Media Links'

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,8 +1,38 @@
 /*typing*/
 
-var typed = new Typed(".typing",{
-    strings:["","Software Engineer","Web Designer","Web Developer","Graphic Designer"],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
-})
+var typed = new Typed(".typing", {
+    strings: ["", "Software Engineer", "Web Designer", "Web Developer", "Graphic Designer"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+});
+
+/*Aside*/
+
+const nav = document.querySelector(".nav"),
+    navList = nav.querySelector("li"),
+    totalNavList = navList.length,
+    allSection = document.querySelector(".section"),
+    totalSection = allSection.length;
+
+for (let i = 0; i < totalNavList; i++) {
+    const a = navList[i].querySelector("a");
+
+    a.addEventListener("click",function (){
+
+        for (let j = 0; j < totalNavList; j++) {
+
+           navList[j].querySelector("a").classList.remove("active");
+        }
+        this.classList.add("active")
+        showSection(this);
+    })
+}
+function showSection(element){
+
+    for (let i = 0; i < totalSection; i++) {
+        allSection[i].classList.remove("active");
+    }
+    const target = element.getAttribute("href").split("#")[1];
+    document.querySelector("#" + target).classList.add("active")
+}
